5b53ac3c7ec236506d40d21d27ca387dcd086e56
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-04-19 22:07:35 +0300

    Create 05-b-2800.sh

---
+++ Solutions/Fourth Part - Shell/05-b-2800.sh
@@ -0,0 +1,16 @@
+-- 05-b-2800
+
+
+#!/bin/bash
+# 05-b-2800 solution
+
+if [ $# -ne 1 ]; then
+        echo "Invalid number of arguments"
+        exit 1
+elif [[ "${1}" =~ [[:alnum:]] ]]; then
+        echo "Valid entry!"
+        exit 0
+else
+        echo "Wrong entry!"
+        exit 2
+fi
