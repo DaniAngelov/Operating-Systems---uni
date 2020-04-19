ee91fae5badffde43550025e654464ad8cb7a79f
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-04-19 22:09:20 +0300

    Create 05-b-3200.sh

---
+++ Solutions/Fourth Part - Shell/05-b-3200.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#-- 05-b-3200
+#Да се напише shell скрипт, който приканва потребителя да въведе пълното име на директория и
+#извежда на стандартния изход подходящо съобщение за броя на всички файлове и всички директории в нея.
+
+read -p "Enter directory: ( Pattern /directory ) " var
+
+if [ ! -d $var ];then
+       echo "This is not a directory! Please enter a valid directory name! "
+else
+        echo "List of directories and subdirectories: "
+        find ${var} type -d
+fi
