89c45b8b4355326cdd9dc1078a34acc5a7957633
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-04-26 14:23:30 +0300

    Update 05-b-3200.sh

--- Solutions/Fourth Part - Shell/05-b-3200.sh
+++ Solutions/Fourth Part - Shell/05-b-3200.sh
@@ -3,11 +3,11 @@
 #Да се напише shell скрипт, който приканва потребителя да въведе пълното име на директория и
 #извежда на стандартния изход подходящо съобщение за броя на всички файлове и всички директории в нея.
 
-read -p "Enter directory: ( Pattern /directory ) " var
+read -p "Enter directory: ( Pattern /directory ) " VAR
 
-if [ ! -d "${var}" ];then
+if [ ! -d "${VAR}" ];then
        echo "This is not a directory! Please enter a valid directory name! "
 else
-        echo "List of directories and subdirectories: "
-        find "${var}" type -d 2>/dev/null | wc -l
+        echo "List of directories : " $(find "${VAR}" type -d 2>/dev/null | wc -l)
+        echo "List of files : " $(find "${VAR}" type -f 2>/dev/null | wc -l)
 fi
