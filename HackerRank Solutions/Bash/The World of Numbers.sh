6b2a5e32bbd1f7a139e1069ff326f97c089c0bed
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-05-11 22:41:11 +0300

    Create The World of Numbers.sh

---
+++ HackerRank Solutions/Bash/The World of Numbers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#github.com/DaniAngelov
+
+read input1
+read input2
+
+VAR=0
+
+VAR=$((input1 + input2))
+echo $VAR
+VAR=$((input1 - input2))
+echo $VAR
+VAR=$((input1 * input2))
+echo $VAR
+VAR=$((input1 / input2))
+echo $VAR
