d3a6f8648c4f15e96d33b1886a1393cafb4508f0
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-05-17 18:30:02 +0300

    Create Task1.c

---
+++ Solutions/C Files/Task1.c
@@ -0,0 +1,45 @@
+
+//  github.com/DaniAngelov
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <err.h>
+#include <stdlib.h>
+
+int main(int argc,char** argv)
+{
+        int fd1;
+        char c;
+        int count = 0;
+
+        if( argc != 2)
+        {
+                errx(1, "Invalid number of arguments");
+        }
+
+        if( (fd1 = open(argv[1],O_RDONLY)) == -1)
+        {
+                err(2, "File is not readable! ");
+        }
+
+        while (read(fd1,&c,1) )
+        {
+                if( c == '\n' )
+                {
+                        count = count + 1;
+                                        }
+
+                write(1,&c,1);
+                if(count == 10)
+                {
+                        break;
+                }
+        }
+
+        close(fd1);
+        exit(0);
+
+}
