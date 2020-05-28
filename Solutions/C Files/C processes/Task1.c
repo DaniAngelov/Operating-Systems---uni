2e4b0d200e2d4fd2fea60812a86b04aba084f81e
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-05-28 19:38:07 +0300

    Update Task1.c

--- Solutions/C Files/C processes/Task1.c
+++ Solutions/C Files/C processes/Task1.c
@@ -1,1 +1,22 @@
-s
+//  github.com/DaniAngelov
+
+/*
+        Task 1
+        Да се напише програма на C, която изпълнява команда date.
+*/
+#include <err.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+        if(execlp("date" ,"date", (char*) NULL) < 0 )
+        {
+                err(1, "Err while exec");
+        }
+
+        exit(0);
+
+
+}
