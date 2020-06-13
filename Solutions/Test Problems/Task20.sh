0c7e797aaea329820b4a09d329036d6ad007ac83
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-13 15:47:48 +0300

    Update Task20.sh

--- Solutions/Test Problems/Task20.sh
+++ Solutions/Test Problems/Task20.sh
@@ -5,20 +5,25 @@
 
 
 if [ $# -ne 3 ]; then
-        echo "Invalid number of arguments! "
+        echo "Invalid number of args"
         exit 1
 fi
 
-if [ ! -d "$1" ] || [ ! -d "$2" ]; then
-        echo "First or second arg not a directory! "
+if [ $(whoami) != "root" ] ; then
+        echo "not the root user"
         exit 2
 fi
 
 SRC="${1}"
-DEST="${2}"
-STRING="${3}"
+DST="${2}"
+STR="${3}"
 
-if [ $(whoami) == "root" ]; then
-        find "${SRC}" -type f -name "${STRING}" -exec cp {} "${DEST}" \;
+FILES=$(find "${SRC}" -type f | grep "${STR}")
+while read file
+do
+        if [ ! -e "${DST}" ]; then
+                mkdir "${DST}"
+        fi
+        mv "$file" "${DST}"
 
-fi
+done < <(echo "${FILES}")
