da3d6d0c6d7e51fe1b24d8b098c6387e4d6c5b27
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-13 15:46:19 +0300

    Update Task14.sh

--- Solutions/Test Problems/Task14.sh
+++ Solutions/Test Problems/Task14.sh
@@ -3,22 +3,40 @@
 #Task14.sh
 #github.com/DaniAngelov
 
-
 if [ $# -ne 1 ]; then
-        echo "Invalid number of arguments! "
+        echo "Invalid number of args"
         exit 1
 fi
 
-ARG="${1}"
+[ $(id -u) -eq 0 ] || exit 2
 
-if [ $(whoami) == "root" ]; then
-        for i in $(ps -eo rss --no-headers)
-        do
-                if [ ${i} -gt ${ARG} ];then
-                        echo "KILL PROCESS"             #KILL PROCESS
-                fi
-        done
-else
-        echo "You're not the root user! "
-        exit 2
-fi
+ARG="${1}"
+PS=$(ps -e -o user | tail -n +2 | sort | uniq)
+
+while read U; do
+        TOTAL_RSS=0
+        U_PR=0
+
+        while read CPID CRSS;do
+
+                U_PR=$(expr ${U_PR} + 1)
+                TOTAL_RSS=$(expr ${TOTAL_RSS} + ${CRSS})
+                MAX_RSS=${CRSS}
+                MAX_RSS_PID=${CPID}
+
+        done < <( ps -u ${U} -o pid,rss | tail -n +2 | sort -n -k 2)
+
+
+
+        if [ "${U_PR}" -eq 0 ]; then
+                continue
+        fi
+
+        if [ "${MAX_RSS}" -gt "${ARG}" ]; then
+                echo "Killing process "${MAX_RSS_PID}" "
+                kill -s SIGTERM "${MAX_RSS_PID}"
+                sleep 2
+                kill -s SIGKILL "${MAX_RSS_PID}"
+        fi
+
+done < <(echo "${PS}" | grep -v "root")
